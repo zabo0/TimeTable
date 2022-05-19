f9832d0eb00b62315510f509186f1c0bc1e4f9b1
Sabahattin Çiçek <scicek12345@gmail.com>
2022-05-19 22:10:26 +0300

    SystemBootReceiver.kt added

---
+++ app/src/main/java/com/saboon/timetable/Notifications/SystemBootReceiver.kt
@@ -0,0 +1,11 @@
+package com.saboon.timetable.Notifications
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class SystemBootReceiver: BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        
+    }
+}
