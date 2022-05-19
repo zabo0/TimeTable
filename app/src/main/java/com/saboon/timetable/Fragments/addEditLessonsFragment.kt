3bfb837951fb2eab9a7b683a42a774886662ece0
Sabahattin Çiçek <scicek12345@gmail.com>
2022-05-19 23:46:14 +0300

    designed add edit fragment

---
+++ app/src/main/java/com/saboon/timetable/Fragments/addEditLessonsFragment.kt
@@ -0,0 +1,26 @@
+package com.saboon.timetable.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.saboon.timetable.R
+
+
+class addEditLessonsFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_edit_lessons, container, false)
+    }
+
+}
